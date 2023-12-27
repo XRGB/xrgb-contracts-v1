30da4a733685f38858d432de35fc488ae2008301
mep <leven@multievent.xyz>
2023-12-27 10:57:00 +0800

    remove useless interface

--- contracts/interfaces/IBRC20Factory.sol
+++ contracts/interfaces/IBRC20Factory.sol
@@ -11,27 +11,4 @@ interface IBRC20Factory {
             uint256 decimals,
             uint256 supply
         );
-
-    function createBRC20(
-        string memory name,
-        string memory symbol,
-        uint8 decimals
-    ) external;
-
-    function mint(
-        address token,
-        address to,
-        uint256 amount,
-        string memory txid,
-        uint8[] memory v,
-        bytes32[] memory r,
-        bytes32[] memory s
-    ) external;
-
-    function burn(
-        address token,
-        uint256 amount,
-        uint8 chainid,
-        string memory receiver
-    ) external;
 }
